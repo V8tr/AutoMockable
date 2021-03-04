62e269ca09d1e52291ec6c4d29dbe098bf80ebc1
V8tr <ne0_n@mail.ru>
2021-03-04 11:41:19 +0200

    Initial commit

---
+++ AutoMockable/AutoMockableApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoMockableApp.swift
+//  AutoMockable
+//
+//  Created by Vadim Bulavin on 04.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct AutoMockableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutoMockable/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AutoMockable
+//
+//  Created by Vadim Bulavin on 04.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
